6f67ad4b2a5b7cabed135e1aafe5a78e7aa4a187
magicland12 <sw_02b@mail.ru>
2017-10-03 19:09:21 +0300

    Initial commit1

---
+++ src/test/java/my/project/ChromeBrowser1.java
@@ -0,0 +1,37 @@
+package my.project;
+
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ChromeBrowser1 {
+
+    public static void main (String[] args){
+
+    System.setProperty("webdriver.chrome.driver", "C:\\SeleniumWebdrivers\\chromedriver.exe");
+    WebDriver driver = new ChromeDriver();
+    driver.manage().window().maximize();
+
+    driver.get("https://rvision.exadel.com");
+    System.out.println ("Site Title is: " + driver.getTitle());
+    driver.findElement(By.xpath("//input[@type='text'][@name='loginForm:login']")).sendKeys("amazur");
+    driver.findElement(By.xpath("//input[@type='password'][@name='loginForm:password']")).sendKeys("Password12");
+    driver.findElement(By.xpath("//input[@type='submit'][@name='loginForm:j_idt39']")).click();
+
+        driver.findElement(By.xpath("//span[text()='Reports']")).click();
+        driver.findElement(By.xpath("//span[text()='Time Offs']")).click();
+
+
+
+        }
+
+}
+
+
+
+
+
